175caab2e09a189cec86f551fa9cd1b9b57feae9
ToonAlfrink <toon.alfrink@gmail.com>
2020-09-16 09:18:10 +0200

    remove json_field dependency

--- executive/settings.py
+++ executive/settings.py
@@ -119,8 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'executive.actions',
-    'json_field',
+    'executive.actions'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
